4fc06ba82dd2a336c0a552bc359fe416fd3714f8
wladerer <wtladerer@gmail.com>
2023-04-03 01:29:26 -0700

    moved plotter helpers to plotter

--- plotter.py
+++ plotter.py
@@ -5,6 +5,51 @@ from plotly.subplots import make_subplots
 import numpy as np
 
 
+def remove_background_and_axes(fig: go.Figure) -> go.Figure:
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        scene=dict(
+            xaxis=dict(
+                visible=False,
+                showticklabels=False,
+                showgrid=False,
+                showspikes=False,
+                showbackground=False,
+                zeroline=False,
+                ticks='',
+            ),
+            yaxis=dict(
+                visible=False,
+                showticklabels=False,
+                showgrid=False,
+                showspikes=False,
+                showbackground=False,
+                zeroline=False,
+                ticks='',
+            ),
+            zaxis=dict(
+                visible=False,
+                showticklabels=False,
+                showgrid=False,
+                showspikes=False,
+                showbackground=False,
+                zeroline=False,
+                ticks='',
+            ),
+        ),
+    )
+
+    return fig
+
+def add_3d_xyz_vectors(fig: go.Figure) -> go.Figure:
+    '''Adds vectors to show the x,y,z axes'''
+    fig.add_trace(go.Scatter3d(x=[0, 0.1], y=[0, 0], z=[0, 0], mode='lines', line=dict(color='red', width=5)))
+    fig.add_trace(go.Scatter3d(x=[0, 0], y=[0, 0.1], z=[0, 0], mode='lines', line=dict(color='green', width=5)))
+    fig.add_trace(go.Scatter3d(x=[0, 0], y=[0, 0], z=[0, 0.1], mode='lines', line=dict(color='blue', width=5)))
+
+    return fig
+
 def filter_bands(df: pd.DataFrame, emin: float = None,  emax: float = None):
 
     if emin is None and emax is None:  # saves time if we don't need to filter
